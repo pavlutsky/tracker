37b9e8e05dc367eee07f645c42d1b42880ba1e83
pavlutsky <pavlutsky@mail.ru>
2022-02-21 16:10:33 +0800

    Вызов метода с аргументами.

--- src/main/java/ru/job4j/tracker/oop/Dog.java
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -1,9 +1,15 @@
 package ru.job4j.tracker.oop;
 
 public class Dog {
+    public String sound() {
+        String voice = "wou-wou";
+        return voice;
+    }
     public static void main(String[] args) {
         Dog polkan = new Dog();
         Dog sharik = new Dog();
         Dog zhychka = new Dog();
+        String say = sharik.sound();
+        System.out.println("Sharik says " + say);
     }
 }
