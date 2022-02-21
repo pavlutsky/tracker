f323f7a93066b64e69be47629daeeccc6042f779
pavlutsky <pavlutsky@mail.ru>
2022-02-21 21:17:48 +0800

    Поля объекта.

--- src/main/java/ru/job4j/tracker/oop/Dog.java
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -1,15 +1,33 @@
 package ru.job4j.tracker.oop;
 
 public class Dog {
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+    }
+
+    void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
-        String voice = "wou-wou";
+        String voice = "Гав-Гав";
         return voice;
     }
+
     public static void main(String[] args) {
         Dog polkan = new Dog();
+        polkan.giveNick("Полкан");
+        polkan.show();
         Dog sharik = new Dog();
-        Dog zhychka = new Dog();
+        sharik.giveNick("Шарик");
+        sharik.show();
         String say = sharik.sound();
-        System.out.println("Sharik says " + say);
+        System.out.println("Шарик лает: " + say);
+        Dog zhychka = new Dog();
+        zhychka.giveNick("Жучка");
+        zhychka.show();
     }
 }
