99b55eec495cb519bd3cd877fead397eb5462715
pavlutsky <pavlutsky@mail.ru>
2022-02-21 21:46:31 +0800

    Взаимодействие объектов.

--- src/main/java/ru/job4j/tracker/oop/Story.java
+++ src/main/java/ru/job4j/tracker/oop/Story.java
@@ -2,11 +2,11 @@ package ru.job4j.tracker.oop;
 
 public class Story {
     public static void main(String[] args) {
-        Girl girl = new Girl();
         Pioneer petya = new Pioneer();
         Wolf wolf = new Wolf();
-        girl.help(petya);
+        Girl girl = new Girl();
         petya.kill(wolf);
         wolf.eat(girl);
+        girl.help(petya);
     }
 }
