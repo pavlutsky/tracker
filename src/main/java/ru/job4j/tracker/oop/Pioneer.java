6034c406ccf9a58a64ff58d2fac86cf6d5e9249d
pavlutsky <pavlutsky@mail.ru>
2022-02-21 21:44:30 +0800

    Поля объекта.

--- src/main/java/ru/job4j/tracker/oop/Pioneer.java
+++ src/main/java/ru/job4j/tracker/oop/Pioneer.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker.oop;
 
 public class Pioneer {
-
     public void kill(Wolf wolf) {
     }
 }
--- src/main/java/ru/job4j/tracker/oop/Story.java
+++ src/main/java/ru/job4j/tracker/oop/Story.java
@@ -2,11 +2,11 @@ package ru.job4j.tracker.oop;
 
 public class Story {
     public static void main(String[] args) {
-        Pioneer petya = new Pioneer();
         Girl girl = new Girl();
+        Pioneer petya = new Pioneer();
         Wolf wolf = new Wolf();
         girl.help(petya);
-        wolf.eat(girl);
         petya.kill(wolf);
+        wolf.eat(girl);
     }
 }
