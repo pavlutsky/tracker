9402ed5d9af9d2191f33bffbe7e5a8f3b6f19fa5
pavlutsky <pavlutsky@mail.ru>
2022-02-21 21:39:02 +0800

    Поля объекта.

--- src/main/java/ru/job4j/tracker/oop/Dog.java
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -16,11 +16,12 @@ public class Dog {
         Dog polkan = new Dog();
         polkan.giveNick("Полкан");
         polkan.show();
-        Dog sharik = new Dog();
-        sharik.giveNick("Шарик");
-        sharik.show();
         Dog zhychka = new Dog();
         zhychka.giveNick("Жучка");
         zhychka.show();
+        Dog sharik = new Dog();
+        sharik.giveNick("Шарик");
+        sharik.show();
+
     }
 }
