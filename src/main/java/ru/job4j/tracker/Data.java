9b2a9b0cdd0c67f31013d1d1bd43506909ff9676
pavlutsky <pavlutsky@mail.ru>
2022-02-22 16:29:57 +0800

    Календарь

---
+++ src/main/java/ru/job4j/tracker/Data.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+import  java.time.*;
+
+public class Data {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+        date = date.minusDays(today - 1);
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+        System.out.println("Пн Вт Ср Чт Пт Сб Вс");
+        for (int i = 1; i < value; i++)
+            System.out.print(" ");
+            while (date.getMonthValue() == month) {
+                System.out.printf("%3d", date.getDayOfMonth());
+                if (date.getDayOfMonth() == today)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+                date = date.plusDays(1);
+                if (date.getDayOfWeek().getValue() == 1)
+                    System.out.println();
+            }
+            if (date.getDayOfWeek().getValue() != 1)
+                System.out.println();
+        }
+}
